6bbc5ef68c9ae65d33a8c9bea522ef19f8bfa12f
Harpreet Singh <harpreetsingh888@gmail.com>
2023-08-05 16:20:58 +1200

    Changes are as follows: - Add initial setup - add reducers, actions and store

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actions } from "./state/index";
+import { accountReducer } from "./state/reducers/account";
+
+function App() {
+  const account = useSelector((state) => {
+    return state.account;
+  });
+
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(actions, dispatch);
+
+  return (
+    <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1)}>Withdraw</button>
+    </div>
+  );
+}
+
+export default App;
